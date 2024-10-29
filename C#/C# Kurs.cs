2467520fb1fb9f1b45fb44fa9154a7bf37ae9b9e
lucanadim <lucanadim@web.de>
2024-10-29 17:21:55 +0100

    C# update 2

--- C#/C# Kurs.cs
+++ C#/C# Kurs.cs
@@ -3,6 +3,7 @@ Console.WriteLine("Hello world");   //---> Hello world
 Console.WriteLine("Can be everything");  //---> Can be everything
 
 
+
 //Variables and data types
 //Variables
 string myName = "Luca";
@@ -26,6 +27,7 @@ string name = "Luca";
 char character = 'L';
 
 
+
 //Calculating in code
 int result = 15 + 2;
 float result = 15f - 2f;
@@ -44,4 +46,42 @@ result2 += 10;
 Console.WriteLine(result2)
 
 
+
 //methods
+void CountToThree()
+{
+    Console.WriteLine(1);
+    Console.WriteLine(2);
+    Console.WriteLine(3);
+    Console.WriteLine("-------");
+}
+CountToThree();
+
+//method parameters
+void SeyHelloTo(string name)
+{
+    Console.WriteLine("Hello" + name);
+    Console.WriteLine("how are you");
+    Consele.WriteLine("-------");
+}
+ SeyHelloTo("Luca")
+
+ //return value
+ double Square(double number6)
+ {
+    double result = number6 * number6;
+    return result;
+ }
+ double quadrat = Square(5);
+ Console.WriteLine(quadrat);
+
+ double CalculateCircumference(double radius)
+ {
+    double result = 2 * Math.PI * radius;
+ }
+ Console.WriteLine(CalculateCircumference(5))
+
+
+
+ //input from the user
+ 
