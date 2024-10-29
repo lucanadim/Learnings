f9112f35443294b377e7787c5bddeaa5824fafd8
lucanadim <lucanadim@web.de>
2024-10-29 17:59:33 +0100

    Phyton update 1

---
+++ Phyton/1/main.py
@@ -0,0 +1,43 @@
+
+print("hallo welt")
+print("print(\"Hallo welt\")")
+print(13)
+print(1 + 1)
+print(1 - 1)
+print(1 * 1)
+print(1 / 1)
+print(1 + 2 - 3 * 4 / 5)
+print((1 + 1) * (2 + 2))
+# kein , sondern .
+print(2.25 + 13)
+
+
+#Praktikum (robi)/(04.07.2023) V1
+
+import os
+
+#def run(file, mode=True):
+    #f = open(file)
+    #s = f.read()
+    #if mode is True:
+        #print(s.upper())
+    #else:
+        #print(s.lower())
+
+
+ #docs = os.listdir("docs")
+ #print(docs)
+
+ #for doc in docs:
+     #file = "docs/"+doc
+     #run(file, mode=True)
+     #run(file, mode=False)
+
+
+
+
+#Praktikum (robi)/(04.07.2023) V2 (rechner)
+
+#while True:
+    #i = input("Rechner: ")
+    #eval(" print( {} ) ".format(i))
