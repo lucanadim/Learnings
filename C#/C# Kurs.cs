b91ed50b70d7c33282862a587fcf774bc6e53513
lucanadim <lucanadim@web.de>
2024-10-29 12:47:47 +0100

    C# update 1

---
+++ C#/C# Kurs.cs
@@ -0,0 +1,47 @@
+//Hello world
+Console.WriteLine("Hello world");   //---> Hello world
+Console.WriteLine("Can be everything");  //---> Can be everything
+
+
+//Variables and data types
+//Variables
+string myName = "Luca";
+Console.WriteLine(myName);   //---> Luca
+
+//date types
+
+//integers
+int number = 20;
+
+//floating-point numbers
+float number1 = 10.3f;
+double number2 = 20.343;
+decimal number3 = 58.23452m;
+
+//truth values
+bool is_adult = false;
+
+//text values
+string name = "Luca";
+char character = 'L';
+
+
+//Calculating in code
+int result = 15 + 2;
+float result = 15f - 2f;
+float result = 15f * 2f;
+float result = 15f / 2f;
+int result = 10 + 15 * 2;
+int result = (10 + 15) * 2;
+Console.WritLine(result);
+
+int number4 = 30;
+int number5 = 20;
+int result2 = number4 + number5;
+
+result2 += 10;
+
+Console.WriteLine(result2)
+
+
+//methods
